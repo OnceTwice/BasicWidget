923c2113c2cf81db6c0933cabc5c79dfa2cbd34f
cho <you-smile@nate.com>
2016-11-26 14:27:12 +0900

    1

--- app/src/main/java/com/bit2016/basicwidget/MainActivity.java
+++ app/src/main/java/com/bit2016/basicwidget/MainActivity.java
@@ -2,6 +2,8 @@ package com.bit2016.basicwidget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("------->", ""+findViewById(R.id.button_invisable).isClickable());
+        findViewById(R.id.button_invisable).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.textView).setVisibility(v.INVISIBLE);
+            }
+        });
+        Log.d("------>", ""+findViewById(R.id.button_invisable).isClickable());
+
+        findViewById(R.id.button_gone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.textView).setVisibility(v.GONE);
+            }
+        });
+
+        findViewById(R.id.button_visible).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.textView).setVisibility(v.VISIBLE);
+            }
+        });
     }
 }
